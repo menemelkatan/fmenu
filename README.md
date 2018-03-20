8590979a92b6a201f965442f0e55b8850f5f61b0
menemelkatan <ccii98@yahoo.com>
2018-03-20 18:40:45 +0200

    allmobile/search/logo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fmenu
+
+## Props
+
+Property|Values|Default|Description
+---|---|---|---
+data-fmenu|-|-|initialize the fmenu
+data-fmenu-dir|rtl, ltr|ltr|The direction of the fmenu
+data-fmenu-fixed|boolean|false|make the fmenu position fixed on screen
+data-fmenu-allMobile|boolean|false|make the fmenu style like mobile all time
+data-fmenu-logo|["string"]logo url|-|put logo inside the fmenu
+data-fmenu-search|["string"]search url|-|add search feature to the fmenu
+data-fmenu-position|bottom, top|top|position of the fmenu
--- js/fmenu.js
+++ js/fmenu.js
@@ -1,5 +1,13 @@
 var fmenu = document.querySelectorAll('[data-fmenu]');
 
+var searchForm = "<input type='text' id='fmenu-search'>" + '<label for="fmenu-search"><i class="fa fa-search" name="q"></i></label>';
+
+
+
+window.onresize= function() {
+  console.log(window.innerWidth)
+}
+
 window.onload = function() {
 
   for(var i = 0; i < fmenu.length; i++){
@@ -10,9 +18,25 @@ window.onload = function() {
   }
 
 
-
   fmenu.forEach(function(el) {
     el.classList.add('fmenu');
+
+    // add the logo
+    if(el.hasAttribute('data-fmenu-logo')){
+      var qqq = document.createElement('img');
+      qqq.className = 'fmenu-logo';
+      qqq.setAttribute('src', ''+el.getAttribute('data-fmenu-logo')+'');
+      el.insertBefore(qqq, el.children[0])
+    }
+
+    //add search
+    if(el.hasAttribute('data-fmenu-search')){
+      var qqq = document.createElement('form');
+      qqq.innerHTML = searchForm;
+      qqq.className = 'fmenu-search';
+      qqq.setAttribute('action', ''+el.getAttribute('data-fmenu-search')+'');
+      el.insertBefore(qqq, el.lastElementChild)
+    }
 
     el.lastElementChild.onclick = function() {
       el.classList.toggle('fmenu-show');
